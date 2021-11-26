ef8fe69f5e01f3ad5581d6561ec83020bfc0f9f2
Anthony Soulier <anthony.soulier@pulselive.com>
2021-11-26 21:31:40 +0000

    add local package structure

---
+++ LocalPackage/BreakingBadData/Tests/BreakingBadDataTests/BreakingBadDataTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BreakingBadData
+
+final class BreakingBadDataTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BreakingBadData().text, "Hello, World!")
+    }
+}
